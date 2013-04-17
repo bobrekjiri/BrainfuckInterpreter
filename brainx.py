5104dd28e1951df661d8f31110106a55a665132f
Jiří Bobrek <bobrekjiri@gmail.com>
2013-04-17 23:44:48 +0200

    Completed BrainCopter

--- brainx.py
+++ brainx.py
@@ -170,10 +170,66 @@ class BrainLoller():
 class BrainCopter():
     """Třída pro zpracování jazyka braincopter."""
 
+    def move(self, x, y, direction):
+        if   direction == 0:
+            x += 1
+            if x ==  self.width:
+                return None, None
+            return x, y
+        elif direction == 1:
+            y += 1
+            if y == self.height:
+                return None, None
+            return x, y
+        elif direction == 2:
+            x -= 1
+            if x == -1:
+                return None, None
+            return x, y
+        else:
+            y -= 1
+            if y == -1:
+                return None, None
+            return x, y
+
     def __init__(self, filename):
         """Inicializace interpretru braincopteru."""
 
-        # self.data obsahuje rozkódovaný zdrojový kód brainfucku..
         self.data = ''
-        # ..který pak předhodíme interpretru
+        print("Decoding image...", end=' ')
+        image = image_png.PngReader(filename)
+        print("Done\nInterpreting...")
+        self.width = image.width
+        self.height = image.height
+        x = 0
+        y = 0
+        direction = 0
+        while 1:
+            cell = image.rgb[y][x]
+            code = (65536 * cell[0] + 256 * cell[1] + cell[2]) % 11
+            if   code == 0:
+                self.data += '>'
+            elif code == 1:
+                self.data += '<'
+            elif code == 2:
+                self.data += '+'
+            elif code == 3:
+                self.data += '-'
+            elif code == 4:
+                self.data += '.'
+            elif code == 5:
+                self.data += ','
+            elif code == 6:
+                self.data += '['
+            elif code == 7:
+                self.data += ']'
+            elif code == 8:
+                direction = (direction + 1) % 4
+            elif code == 9:
+                direction = 3 if direction == 0 else direction - 1
+
+            x, y = self.move(x, y, direction)
+            if x == None:
+                break
+        print("Done")
         self.program = BrainFuck(self.data)
