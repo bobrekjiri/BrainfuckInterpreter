d88e91d37920d7c958c8a4faddc2833b2933fd8e
Jiří Bobrek <bobrekjiri@gmail.com>
2013-04-17 19:14:13 +0200

    Removed trailing whitespaces

--- brainx.py
+++ brainx.py
@@ -6,13 +6,13 @@ import image_png
 
 class BrainFuck:
     """Interpretr jazyka brainfuck."""
-    
+
     def getInput(self, data):
         tmp = data.split("!")
         if len(tmp) == 1:
             return ""
         return tmp[1]
-    
+
     def __init__(self, data, memory=b'\x00', memory_pointer=0):
         """Inicializace interpretru brainfucku."""
         stack = list()
@@ -21,19 +21,19 @@ class BrainFuck:
         inputData = self.getInput(data)
         inputPointer = 0
         dataLength = len(self.data)
-        
+
         # inicializace proměnných
         self.memory = bytearray(memory)
         self.memory_pointer = memory_pointer
         memoryLength = len(self.memory)
-        
+
         # DEBUG a testy
         # a) paměť výstupu
         self.output = ''
-    
+
         while 1:
             x = self.data[dataPointer]
-            if x == '+':
+            if   x == '+':
                 tmp = self.memory[self.memory_pointer]
                 tmp = (tmp + 1) % 256
                 self.memory[self.memory_pointer] = tmp
@@ -78,7 +78,7 @@ class BrainFuck:
                 self.output = self.output + tmp
                 sys.stdout.write(tmp)
                 sys.stdout.flush()
-                
+
             elif x == ',':
                 if inputPointer == len(inputData):
                     tmp = ord(sys.stdin.read(1))
@@ -86,8 +86,8 @@ class BrainFuck:
                     tmp = ord(inputData[inputPointer])
                     inputPointer += 1
                 self.memory[self.memory_pointer] = tmp % 256
-            
-            dataPointer += 1;            
+
+            dataPointer += 1;
             if dataPointer >= dataLength:
                 break
 
@@ -100,13 +100,12 @@ class BrainFuck:
 
 class BrainLoller():
     """Třída pro zpracování jazyka brainloller."""
-    
+
     def __init__(self, filename):
         """Inicializace interpretru brainlolleru."""
-        
+
         image = image_png.PngReader(filename)
-        
-        
+
         # self.data obsahuje rozkódovaný zdrojový kód brainfucku..
         self.data = ''
         # ..který pak předhodíme interpretru
@@ -115,13 +114,11 @@ class BrainLoller():
 
 class BrainCopter():
     """Třída pro zpracování jazyka braincopter."""
-    
+
     def __init__(self, filename):
         """Inicializace interpretru braincopteru."""
-        
+
         # self.data obsahuje rozkódovaný zdrojový kód brainfucku..
         self.data = ''
         # ..který pak předhodíme interpretru
         self.program = BrainFuck(self.data)
-
-
